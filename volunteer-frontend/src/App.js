7ccc067cdf3db128690b6b7a46063bcaf86eba15
anuthapaliy <113211321+anuthapaliy@users.noreply.github.com>
2023-09-27 19:54:13 +0100

    deploy

--- volunteer-frontend/src/App.js
+++ volunteer-frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
 useEffect(() => {
 const fetchSessions= async () => {
   try {
-  const response = await fetch('http://localhost:8000/sessions');
+  const response = await fetch('https://project-practice-week1.onrender.com/sessions');
   if (response.ok) {
   const data = await response.json();
   setSessions(data);
